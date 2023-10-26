416faf44918619e9bb67d1cc05ea14550cb61a25
A.Herrera <aherrerac@alumnes.santjosepobrer.es>
2023-10-26 16:39:57 +0200

    Arreglito

--- Python/Actividades de PSeINT/Actividad_6/Ejercicio_4.py
+++ Python/Actividades de PSeINT/Actividad_6/Ejercicio_4.py
@@ -3,8 +3,11 @@ sumaNums = 0
 cantidadNums = 0
 while num != -1:
     num = float(input('Introduce un número: '))
-    cantidadNums += 1
-    sumaNums += num
+    if num != -1 and num > -1:
+        cantidadNums += 1
+        sumaNums += num
+    else:
+        print('Inserte un número válido')
 
 resultado = sumaNums/cantidadNums
 print(f"La mediana aritmética es {resultado}")
