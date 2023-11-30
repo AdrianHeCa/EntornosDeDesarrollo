1d80711a5bcf23316df832417708d350a91a0467
A.Herrera <aherrerac@alumnes.santjosepobrer.es>
2023-11-30 17:44:47 +0100

    avion online es un mmorpg no lineal en el que cons

---
+++ Java/Java/src/App.java
@@ -0,0 +1,73 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("I don't really want to go\r\n" + //
+                "I don't really wanna stay\r\n" + //
+                "But I really hope and pray\r\n" + //
+                "Can we get it together\r\n" + //
+                "Get it together\r\n" + //
+                "(Mustard on the beat, ho)\r\n" + //
+                "I put the new Forgi's on the Jeep\r\n" + //
+                "I trap until the bloody bottoms is underneath\r\n" + //
+                "'Cause all my niggas got it out the streets\r\n" + //
+                "I keep a hunnid racks inside my jeans\r\n" + //
+                "I remember hittin' the mall with the whole team\r\n" + //
+                "Now a nigga can't answer calls 'cause I'm ballin'\r\n" + //
+                "I was wakin' up, gettin' racks in the mornin'\r\n" + //
+                "I was broke now I'm rich, these niggas salty\r\n" + //
+                "All this designer on my body got me drip, drip (ayy)\r\n" + //
+                "Straight up out the Yajects, I'm a big Crip\r\n" + //
+                "If I got a pint of lean, I'ma sip, sip\r\n" + //
+                "I run the racks up with my queen, like London and Nip\r\n" + //
+                "But I got rich on all these niggas, I didn't forget, bet\r\n" + //
+                "I had to go through the struggle, I didn't forget that\r\n" + //
+                "I hopped inside of the Maybach and now I can sit back\r\n" + //
+                "These bitches know me now, 'cause I got them big racks\r\n" + //
+                "'Cause I'm gettin' money now, I know you heard that\r\n" + //
+                "Young nigga on the corner, bitch, I had to serve crack\r\n" + //
+                "Uncle fronted me some P's, had to get them birds back\r\n" + //
+                "We came up on dirty money, I gave it a bird bath\r\n" + //
+                "Cut off the brain and I give my bitch a new coupe\r\n" + //
+                "Either you frontin' Yah Gang or you're SuWoop\r\n" + //
+                "Got a New Orleans bitch and man that pussy voodoo\r\n" + //
+                "And I'm that nigga now, who knew?\r\n" + //
+                "I put the new Forgi's on the Jeep\r\n" + //
+                "I trap until the bloody bottoms is underneath\r\n" + //
+                "'Cause all my niggas got it out the streets\r\n" + //
+                "I keep a hunnid racks, inside my jeans\r\n" + //
+                "I remember hittin' the mall with the whole team\r\n" + //
+                "Now a nigga can't answer calls 'cause I'm ballin'\r\n" + //
+                "I was wakin' up gettin' racks in the mornin'\r\n" + //
+                "I was broke now I'm rich, these niggas salty\r\n" + //
+                "I been wakin' up to get the money, whoa, whoa\r\n" + //
+                "Got a bad bitch, her ass tatted, whoa, whoa\r\n" + //
+                "Givenchy to my toes, two twins, I'm fuckin' 'em both\r\n" + //
+                "I put her in new AP, the water like a boat\r\n" + //
+                "I was down bad on my dick, where was you niggas at?\r\n" + //
+                "I know you turned your back on me, just to get some racks\r\n" + //
+                "I seen you swerve back, 'cause I'm in the black 'Bach\r\n" + //
+                "New diamonds on me, fuck a flash, this ain't SnapChat\r\n" + //
+                "'Cause I been gettin' paid\r\n" + //
+                "Yellow diamonds on me look like lemonade\r\n" + //
+                "Got my baby mama that new Bentayga\r\n" + //
+                "Tryna get the dough, Joe, like a sensei, yeah\r\n" + //
+                "Rolls Royce umbrellas when I'm in the rain\r\n" + //
+                "I just mind my business\r\n" + //
+                "I got brothers that did the time, I ain't kiddin'\r\n" + //
+                "All these rappers just talk about it, I live it\r\n" + //
+                "Goin' up, I ain't got no sky limit, yeah, yeah, yeah\r\n" + //
+                "I put the new Forgi's on the Jeep\r\n" + //
+                "I trap until the bloody bottoms is underneath\r\n" + //
+                "'Cause all my niggas got it out the streets\r\n" + //
+                "I keep a hunnid racks inside my jeans\r\n" + //
+                "I remember hittin' the mall with the whole team\r\n" + //
+                "Now a nigga can't answer calls 'cause I'm ballin'\r\n" + //
+                "I was wakin' up gettin' racks in the mornin'\r\n" + //
+                "I was broke now I'm rich, these niggas salty\r\n" + //
+                "I, ayy, yeah\r\n" + //
+                "I've been ballin', lil' nigga\r\n" + //
+                "Now watch me ball on these niggas\r\n" + //
+                "Yeah, now watch me ball on these niggas, yeah\r\n" + //
+                "Now watch me ball on these niggas\r\n" + //
+                "Now watch me ball on these niggas, yeah, yeah");
+    }
+} 
